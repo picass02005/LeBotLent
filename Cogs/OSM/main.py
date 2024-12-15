c861bceb5285a32c0c6cee2581d8885cbdd93a3b
picasso2005 <clementduran0@gmail.com>
2024-12-15 18:42:54 +0100

    Added OSM's cog

---
+++ Cogs/OSM/main.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+from discord.ext import commands
+
+
+# === DO NOT CHANGE CLASS NAME OR __init__ PARAMETERS === #
+class OSM(commands.GroupCog):
+    def __init__(self, bot: commands.AutoShardedBot, database: sqlite3.Connection):
+        self.bot: bot = bot
+        self.database = database
+
+
+# === DO NOT REMOVE THE FOLLOWING OR CHANGE PARAMETERS === #
+
+async def setup(bot: commands.AutoShardedBot, database: sqlite3.Connection):
+    await bot.add_cog(OSM(bot, database))
