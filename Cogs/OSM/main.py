6906660787816121840be02eb50e7ed20589ab25
picasso2005 <clementduran0@gmail.com>
2025-02-06 19:20:15 +0100

    Added methods to fetch changes / notes number since a datetime + implemented them in DB

--- Cogs/OSM/main.py
+++ Cogs/OSM/main.py
@@ -3,9 +3,11 @@
 
 import sqlite3
 import time
+from datetime import datetime
 
 from discord.ext import commands, tasks
 
+from Cogs.OSM.GetChangesNotesNb import get_notes_nb, get_changes_nb
 from Cogs.OSM.Py_OSM_API import PyOSM, py_osm_builder
 from GlobalModules.GetConfig import get_config
 
@@ -39,6 +41,8 @@ class OSM(commands.GroupCog):
             "TIMESTAMP UNSIGNED INT(10),"
             "OSM_UID INTEGER,"
             "CHANGESET_NB INTEGER,"
+            "CHANGES_NB INTEGER,"
+            "NOTES_NB INTEGER,"
             "TRACES_NB INTEGER,"
             "BLOCKS_NB INTEGER,"
             "BLOCKS_ACTIVE INTEGER);"
@@ -58,13 +62,33 @@ class OSM(commands.GroupCog):
         ts = int(time.time())
 
         for i in users:
+            cursor = self.database.execute(
+                "SELECT TIMESTAMP, CHANGESET_NB, CHANGES_NB FROM OSM_LEADERBOARD_DATA "
+                "WHERE OSM_UID=? ORDER BY TIMESTAMP DESC LIMIT 1;",
+                (i.uid,)
+            ).fetchone()
+
+            last_timestamps = datetime.fromtimestamp(cursor[0] + 1 if cursor is not None else 0)
+            last_changeset_nb = cursor[1] if cursor is not None else 0
+            last_changes_nb = cursor[2] if cursor is not None else 0
+
+            if i.changesets_count != last_changeset_nb:
+                changes_nb = await get_changes_nb(self.py_osm, i.uid, last_timestamps)
+
+            else:
+                changes_nb = last_changes_nb
+
+            notes_nb = await get_notes_nb(self.py_osm, i.uid, last_timestamps)
+
             self.database.execute(
-                "INSERT INTO OSM_LEADERBOARD_DATA (TIMESTAMP, OSM_UID, CHANGESET_NB, TRACES_NB, BLOCKS_NB, "
-                "BLOCKS_ACTIVE) VALUES (?, ?, ?, ?, ?, ?);",
+                "INSERT INTO OSM_LEADERBOARD_DATA (TIMESTAMP, OSM_UID, CHANGESET_NB, CHANGES_NB, NOTES_NB, "
+                "TRACES_NB, BLOCKS_NB, BLOCKS_ACTIVE) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
                 (
                     ts,
                     i.uid,
                     i.changesets_count,
+                    changes_nb,
+                    notes_nb,
                     i.traces_count,
                     i.blocks_count,
                     i.blocks_active
@@ -89,4 +113,6 @@ async def setup(bot: commands.AutoShardedBot, database: sqlite3.Connection):
 # TODO: Search element
 # TODO: show map
 
-# TODO: When adding a user in DB, also fetch old changeset count
+# TODO: When adding a user in DB, also fetch old changeset count + notes + nodes count
+# print(await get_notes_nb(py_osm, 14112053))
+# print(await get_changes_nb(py_osm, 14112053))
